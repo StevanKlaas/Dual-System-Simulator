4d0e0ba77ad4bc45417a6667c10ec50141344280
StevanKlaas <70848201+StevanKlaas@users.noreply.github.com>
2026-07-03 12:50:47 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import Sim from './Simulator'
 
-createRoot(document.getElementById('root')!).render(<Sim />)
+createRoot(document.getElementById('root')!).render(
+  <>
+    <Sim />
+    <Analytics />
+    <SpeedInsights />
+  </>
+)
